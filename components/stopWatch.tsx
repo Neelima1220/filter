40475fd415a50a7957d8f5abe02feaade8061fa1
Neelima1220 <neelimareddy1220@gmail.com>
2022-07-15 23:09:11 +0300

    1 file modified

--- components/stopWatch.tsx
+++ components/stopWatch.tsx
@@ -2,6 +2,10 @@ import React, { useEffect, useState } from 'react';
 
 const StopWatch = () => {
   const [date, setDate] = useState('');
+  const [hour, setHour] = useState(0);
+  const [min, setMin] = useState(0);
+  const [sec, setSec] = useState(0);
+  const [started, setStarted] = useState(false);
 
   useEffect(() => {
     const getTime = () => {
@@ -12,12 +16,22 @@ const StopWatch = () => {
     }, 1000);
     return () => clearInterval(intervel);
   }, []);
-  console.log(date);
+
+  const startTimer = () => {
+    let hours, mins, secs;
+    if (started) {
+      setSec(sec + 1);
+      if (sec > 60) {
+        setMin(min + 1);
+        setSec(0);
+      }
+    }
+  };
   return (
     <div style={{ textAlign: 'center' }}>
       <h4>{date} </h4>
       <h1>Stop watch </h1>
-      <p>hh:mm:ss </p>
+      <p>{`${hour}:${min}:${sec}`}</p>
       <button>Start</button>
     </div>
   );
